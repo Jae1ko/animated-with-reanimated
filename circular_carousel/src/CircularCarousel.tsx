2e17ab1b8d5ba6e3eec9594d0e9d932e5f1baa4f
gojae Lets <gojaelets@gojaeui-MacBookPro.local>
2023-11-16 20:19:25 +0900

    update: circular_carousel

--- circular_carousel/src/CircularCarousel.tsx
+++ circular_carousel/src/CircularCarousel.tsx
@@ -1,13 +1,24 @@
 import { FlatList, ImageProps, View } from 'react-native';
-import { CircularCarouselListItem } from './listItem';
+import { CircularCarouselListItem, ListItemWidth } from './listItem';
+import { useSharedValue } from 'react-native-reanimated';
 
 type CircularCarouselProps = { data: ImageProps['source'][] };
 
 const CircularCarousel: React.FC<CircularCarouselProps> = ({ data }) => {
+    const contentOffset = useSharedValue(0);
+
     return (
         <FlatList
             data={data}
             keyExtractor={(_, index) => index.toString()}
+            scrollEventThrottle={16}
+            onScroll={event => {
+                contentOffset.value = event.nativeEvent.contentOffset.x;
+                // console.log(event.nativeEvent.contentOffset.x);
+            }}
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            snapToInterval={ListItemWidth}
             style={{
                 position: 'absolute',
                 bottom: 0,
@@ -16,11 +27,16 @@ const CircularCarousel: React.FC<CircularCarouselProps> = ({ data }) => {
             contentContainerStyle={{
                 justifyContent: 'center',
                 alignItems: 'center',
+                paddingRight: 3 * ListItemWidth,
             }}
             horizontal
             renderItem={({ item, index }) => {
                 return (
-                    <CircularCarouselListItem imageSrc={item} index={index} />
+                    <CircularCarouselListItem
+                        contentOffset={contentOffset}
+                        imageSrc={item}
+                        index={index}
+                    />
                 );
             }}
         />
